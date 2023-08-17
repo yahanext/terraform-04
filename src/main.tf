73322058dffb4cda614310a76c04a475e698e865
Yaha Vibe <yahanext@gmail.com>
2023-08-17 11:17:38 +0500

    зд 2

--- src/main.tf
+++ src/main.tf
@@ -1,40 +1,16 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">=0.13"
-}
-
-provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
-}
-
-
-
-#создаем облачную сеть
-resource "yandex_vpc_network" "develop" {
-  name = "develop"
-}
-
-#создаем подсеть
-resource "yandex_vpc_subnet" "develop" {
-  name           = "develop-ru-central1-a"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["10.0.1.0/24"]
+module "vpc" {
+  source    = "./vpc"
+  env_name  = "develop"
+  zone      = "ru-central1-a"
+  cidr      = "10.0.1.0/24"
 }
 
 module "test-vm" {
   source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name        = "develop"
-  network_id      = yandex_vpc_network.develop.id
+  network_id      = module.vpc.vpc_id
   subnet_zones    = ["ru-central1-a"]
-  subnet_ids      = [ yandex_vpc_subnet.develop.id ]
+  subnet_ids      = [ module.vpc.subnet_id ]
   instance_name   = "web"
   instance_count  = 1
   image_family    = "ubuntu-2004-lts"
--- src/vpc/main.tf
+++ src/vpc/main.tf
@@ -12,7 +12,7 @@ resource "yandex_vpc_network" "net_name" {
 }
 
 resource "yandex_vpc_subnet" "subnet_name" {
-  name           = join("-", [var.env_name, var.zone])
+  name           = "${var.env_name}-${var.zone}"
   zone           = var.zone
   network_id     = yandex_vpc_network.net_name.id
   v4_cidr_blocks = [var.cidr]
